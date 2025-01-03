0fc1ea7aef309b378e30dfa644648bcb11f15b3e
surajislam <154444093+surajislam@users.noreply.github.com>
2025-01-03 12:20:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,5 +11,5 @@ API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
-OWNER_ID = int(getenv("OWNER_ID", 5059737154))
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/BWANDARLOK")
+OWNER_ID = int(getenv("OWNER_ID", 6903379369))
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/ABOUT_NIGHT_SQUAD")
